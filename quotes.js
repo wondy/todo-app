78c92723ef963ab3ed9867f067af28fdb5f43df3
wondy <wondy1972@me.com>
2019-06-06 09:16:57 +0200

    jquery to the rescue

--- quotes.js
+++ quotes.js
@@ -1,3 +1,9 @@
+// check for jquery
+if (typeof jQuery == 'undefined') {
+ alert("jquery not loaded")
+}
+
+
 
 const app = document.getElementById('quote');
 
@@ -6,29 +12,53 @@ container.setAttribute('class', 'header');
 
 app.appendChild(container);
 
-var request = new XMLHttpRequest();
-request.open('GET', 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', true);
-request.onload = function () {
-
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(quotes => {
+$.ajax({
+    url: 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?',
+    dataType: 'jsonp',
+    success: function(data) {
+      // your code to handle data here
+     // var data = JSON.parse(this.response);
+  
+    
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = quotes.quoteText;
+      h1.textContent = data.quoteText;
   
       container.appendChild(card);
       card.appendChild(h1);   
       card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('div');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-}
+    }
+});
+  
 
-request.send();
+    
+  
+
+// var request = new XMLHttpRequest();
+// request.open('GET', 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?', true);
+// request.onload = function () {
+
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(quotes => {
+//       const card = document.createElement('div');
+//       card.setAttribute('class', 'card');
+
+//       const h1 = document.createElement('h1');
+//       h1.textContent = quotes.quoteText;
+  
+//       container.appendChild(card);
+//       card.appendChild(h1);   
+//       card.appendChild(p);
+//     });
+//   } else {
+//     const errorMessage = document.createElement('div');
+//     errorMessage.textContent = `Gah, it's not working!`;
+//     app.appendChild(errorMessage);
+//   }
+// }
+
+// request.send();
